7c2d83d81e508d2ffe5d94988f02ab032ee255a7
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-10-30 18:53:44 +0500

    Add backend functionality of application

--- app/src/main/java/com/example/random_number/MainActivity.java
+++ app/src/main/java/com/example/random_number/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.random_number;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button rollButton;
+    TextView resultsTextView;
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollButton = findViewById(R.id.roll_button);
+        resultsTextView = findViewById(R.id.result_textview);
+        seekBar = findViewById(R.id.seekBar);
+
+        rollButton.setOnClickListener(v -> {
+         resultsTextView.setText( (new Random().nextInt(seekBar.getProgress())));
+        });
     }
 }
